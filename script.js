4a251722f55641df1b58c3b41cf97da2cb2125a3
Taikimen52 <5taikiiida@gmail.com>
2025-09-22 10:19:58 +0900

    Edit index.html, style.css, script.js Adjusting calcurating functions, now trying to install state management.

--- script.js
+++ script.js
@@ -1,45 +1,58 @@
 //必要なグローバル関数、一つ目の入力数a,二つ目の入力数b
-let a = 0;
-let b =0;
-let operator = ""
+let inputA = 0;
+let inputB = 0;
+let answer = 0;
+let op = ""
 
 const display = document.querySelector(".display");
 
-// 各演算関数
-const add = (a, b) => a + b;
-const subtract = (a, b) => a-b;
-const multiply = (a, b) => a*b;
-const division = (a, b) => a/b;
+const calculator = {
+  displayValue: '0',
+  firstOperand: null,
+  waitingForOperand: false,
+  operator: null
+};
 
+display.innerText = calculator.displayValue;
 
+// 各演算関数をオブジェクトに
+const operations ={
+    "+" : (a, b) => a + b,
+    "-" : (a, b) => a - b,
+    "*" : (a, b) => a * b,
+    "/" : (a, b) => a / b
+}
 
 //number buttons
 const num = document.querySelectorAll(".num");
 num.forEach(el => {
     el.addEventListener("click", ()=>{
-        display.innerText += el.innerText;
+        calculator.displayValue += el.value;
     })
 })
 
-//ACボタン
-const acBtn = document.querySelector(".ac");
-acBtn.addEventListener("click", ()=>{
-    a = 0;
-    b = 0;
-    display.innerText = "";
-});
+//リセット関数
+function reset(){
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitingForOperand = false;
+    calculator.operator = null;
+}
 
-const addBtn = document.querySelector("#plus");
-addBtn.addEventListener("click",()=>{
-    a = parseFloat(display.innerText)
-    display.innerText += "+";
-    op = "+";
-    console.log(a)
-    console.log(typeof a)
+//ACボタンにリセット関数をセット
+const acBtn = document.querySelector(".ac");
+acBtn.addEventListener("click", reset());
+
+//演算子をクリックした時の挙動
+const opBtn = document.querySelectorAll(".op");
+opBtn.forEach(el => {
+    el.addEventListener("click",()=>{
+        inputA = parseFloat(display.innerText);
+        op = el.innerText;
+    })
 });
 
 const eqBtn = document.querySelector("#eq");
 eqBtn.addEventListener("click", ()=>{
-    b = display.innerText;
-    console.log(op)
+    console.log("eq")
 });
